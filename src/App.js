756f90f4cee68b27cad0fcefe3ddb01560b558af
abrknine <abhishekbahukhandi2020@gmail.com>
2023-05-27 23:13:41 +0530

    uiupdate

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='App'> 
-       <aside className='sidemenu '>
-        <div className='side-menu-button'>
+    <div className="App">
+      <aside className="sidemenu ">
+        <div className="side-menu-button">
           <span>+</span>
-             New-chat
+          New-chat
         </div>
-       </aside>
-       <section className='chatbox'>
-          <div className='chat-log'>
-            <div className='chat-message'> 
-              <div className='avatar'>
-                
-    me
-              </div>
-              <div className='message'>
-  hello
-              </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar">me</div>
+
+              <div className="message">hello</div>
             </div>
-
           </div>
-        <div className='chat-input-holder'>
-          <textarea className='chat-input-textarea' placeholder='Text your message here'></textarea>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center ">
+              <div className="avatar">me</div>
 
+              <div className="message">I am bot dfwfew</div>
+            </div>
+          </div>
+          
         </div>
-       </section>
-      
-
+        <div className="chat-input-holder">
+          <textarea
+            className="chat-input-textarea"
+            placeholder="Text your message here"
+          ></textarea>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
